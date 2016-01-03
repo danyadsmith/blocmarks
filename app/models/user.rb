2693fa800704942cd0634be33eee7b18ec680035
Danya D. Smith <danyadsmith@gmail.com>
2016-01-03 02:26:18 -0500

    Generate Topic Model and Associate with Users
    In this commit, we generate the Topic model with the following
    properties:
    
    • string: title
    • references: user
    
    Next, we alter the User model to include a has_many association:
    
    • has_many :topics

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,6 @@ class User < ActiveRecord::Base
   validates :first_name, :presence => true
   validates :last_name, :presence => true
   validates :username, :presence => true, :uniqueness => true
+
+  has_many :topics
 end
