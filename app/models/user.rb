43a64b1d1d0a5cd6fac0063d02ceb3d040f71766
Danya D. Smith <danyadsmith@gmail.com>
2016-01-03 01:57:30 -0500

    Configure Devise Confirmable via Email
    In this commit, we modify our initial devise_create_users migration to
    add an index to the username field, then we create a new migration
    called add_confirmable_to_devise. We add the required fields for a
    confirmable devise user.
    
    We update the user model to make sure that:
    • First Name is required
    • Last Name is required
    • Username is required and must be unique
    
    Next, we modify the development.rb file to include smtp_settings for
    action_mailer.
    
    Next, we update the secrets.yml file to include environment variables
    used in the smtp_settings configuration, including:
    • domain_name
    • email_provider_username
    • email_provider_password
    
    Finally, we update the application controller to include a before_filter
    that configures our custom devise parameters.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  attr_accessor :username, :first_name, :middle_initial, :last_name
 
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
+  validates :username, :presence => true, :uniqueness => true
 end
