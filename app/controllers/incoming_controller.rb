71a2e2a969c7619d23c3089746cee0505ff1aec5
Danya D. Smith <danyadsmith@gmail.com>
2016-01-26 20:52:34 -0500

    Second Test of Incoming_Controller
    Here, we add a test to see if the Topic exists. If not, we create the
    topic before creating the bookmark.

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -4,8 +4,15 @@ class IncomingController < ApplicationController
     puts params.inspect
 
     # Get Bookmark Data from Email Message
-    user = User.where(email: params['sender']).first
-    topic = Topic.where(topic: params['Subject']).first
+    user = User.where(email: params["sender"]).first
+    topic = Topic.where(topic: params["Subject"]).first
+
+    unless topic do
+      new_topic = Topic.new
+      new_topic.user_id = user.id
+      new_topic.title = params["Subject"]
+    end
+
     bookmark_data = params["stripped-text"].split("\r\n")
     
     bookmark_link_text = bookmark_data[0] 
