3474d77439de90dd749b3d349ff184597e7da3e0
Danya D. Smith <danyadsmith@gmail.com>
2016-01-26 21:37:45 -0500

    Adding Output to Logs for Error Checking in Incoming Controller

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -3,30 +3,38 @@ class IncomingController < ApplicationController
   def create
     puts params.inspect
 
-    # Get Bookmark Data from Email Message
+    puts "--- Get User and Topic from Incoming Email ---"
     user = User.where(email: params["sender"]).first
-    topic = Topic.where(topic: params["Subject"]).first
 
-    if topic.nil?
-      new_topic = Topic.new
-      new_topic.user_id = user.id
-      new_topic.title = params["Subject"]
-      new_topic.save
+    if user.nil?
+      # Do Nothing
+    else
+      puts "User: #{user.username}"
+      topic = Topic.where(topic: params["Subject"]).first
+      puts "Topic: #{topic.title}"
+
+      if topic.nil?
+        puts "--- Creating New Topic ---"
+        new_topic = Topic.new
+        new_topic.user_id = user.id
+        new_topic.title = params["Subject"]
+        new_topic.save
+      end
+
+      puts "--- Attempting to Create New Bookmark ---"
+      bookmark_data = params["stripped-text"].split("\r\n")
+      
+      bookmark_link_text = bookmark_data[0] 
+      bookmark_url = bookmark_data[1] 
+      bookmark_description = bookmark_data[2] 
+
+      new_bookmark = Bookmark.new
+      new_bookmark.topic_id = topic.id 
+      new_bookmark.url = bookmark_url
+      new_bookmark.url_link_text = bookmark_link_text
+      new_bookmark.description = bookmark_description
+      new_bookmark.save
+
     end
-
-    bookmark_data = params["stripped-text"].split("\r\n")
-    
-    bookmark_link_text = bookmark_data[0] 
-    bookmark_url = bookmark_data[1] 
-    bookmark_description = bookmark_data[2] 
-
-    new_bookmark = Bookmark.new
-    new_bookmark.topic_id = topic.id 
-    new_bookmark.url = bookmark_url
-    new_bookmark.url_link_text = bookmark_link_text
-    new_bookmark.description = bookmark_description
-    new_bookmark.save
-
-
   end
 end
