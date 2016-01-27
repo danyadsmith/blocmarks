7061e481d5a0fb9fb701aec6c3285acaa9967e8d
Danya D. Smith <danyadsmith@gmail.com>
2016-01-26 20:33:47 -0500

    Test Implmentation of Incoming Controller
    Confirming that parsed data from an incoming e-mail can be captured and
    saved.

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -2,10 +2,23 @@ class IncomingController < ApplicationController
 
   def create
     puts params.inspect
-    #params['sender']
-    # user = User.where(email: params['sender']).first
-    # unless user, create topic, bookmark
-    #params[:subject]
-    #params['body-plain'] or params["stripped-text"]
+
+    # Get Bookmark Data from Email Message
+    user = User.where(email: params['sender']).first
+    topic = Topic.where(topic: params['Subject']).first
+    bookmark_data = params["stripped-text"].split("\r\n")
+    
+    bookmark_link_text = bookmark_data[0] 
+    bookmark_url = bookmark_data[1] 
+    bookmark_description = bookmark_data[2] 
+
+    new_bookmark = Bookmark.new
+    new_bookmark.topic_id = topic.id 
+    new_bookmark.url = bookmark_url
+    new_bookmark.url_link_text = bookmark_link_text
+    new_bookmark.description = bookmark_description
+    new_bookmark.save
+
+
   end
 end
